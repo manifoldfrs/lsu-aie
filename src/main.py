44a320c4a9cdf120e6130b315e112ad1af27e92f
Faris <frs@hbb.dev>
2024-05-05 21:07:53 -0700

    add speech to text

--- src/main.py
+++ src/main.py
@@ -163,10 +163,25 @@ async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
 
+async def transcribe_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Make sure we have a voice file to transcribe
+    voice_id = update.message.voice.file_id
+    if voice_id:
+        file = await context.bot.get_file(voice_id)
+        await file.download_to_drive(f"voice_note_{voice_id}.ogg")
+        await update.message.reply_text("Voice note downloaded, transcribing now")
+        audio_file = open(f"voice_note_{voice_id}.ogg", "rb")
+        transcript = openai.audio.transcriptions.create(
+            model="whisper-1", file=audio_file
+        )
+        await update.message.reply_text(f"Transcript finished:\n {transcript.text}")
+
+
 if __name__ == "__main__":
     application = ApplicationBuilder().token(tg_bot_token).build()
 
     chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
+    voice_handler = MessageHandler(filters.VOICE, transcribe_message)
     start_handler = CommandHandler("start", start)
     image_handler = CommandHandler("image", image)
     mozilla_handler = CommandHandler("mozilla", mozilla)
@@ -175,5 +190,6 @@ if __name__ == "__main__":
     application.add_handler(chat_handler)
     application.add_handler(image_handler)
     application.add_handler(mozilla_handler)
+    application.add_handler(voice_handler)
 
     application.run_polling()
